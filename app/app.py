d66e23e51e28ed08186105765d3f26340e5013ac
devopseusr <archanapradeep282@gmail.com>
2025-10-12 21:58:40 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,13 @@
+# app/app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    image_tag = os.environ.get("IMAGE_TAG", "dev")
+    return f"Hello, World! (image: {image_tag})\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
